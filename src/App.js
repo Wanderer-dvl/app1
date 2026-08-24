3babcc19e8f344f9282b88dfb48472155589e665
Wanderer <102596701+Wanderer-dvl@users.noreply.github.com>
2026-08-24 10:06:21 +0700

    create useEffect  useState file

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import RefsFunc from './ref-func';
 import RefsArray from './refs-array';
 import MessageBox from './state-class';
+import Effect from './effect';
 
 function App() {
   
   
-  return <MessageBox/>
+  return <Effect/>
 }
 
 export default App;
---
+++ src/effect.js
@@ -0,0 +1,19 @@
+import React,{ useEffect,useState} from "react";
+
+export default function Effect() {
+    const [count,setCount]= useState(0)
+    const [name,setName]= useState("John Wick")
+
+    useEffect(()=>{
+        document.title=`You clicked ${count} times`
+    },[count])
+    return (
+        <center>
+            <h1>{count}</h1>
+            <h3>{name}</h3>
+            <button onClick={()=>setCount(count+1)}>Increase</button>
+            <button onClick={()=>setCount(count-1)}>Decrease</button>
+            <button onClick={()=>setName("Jack Wick")}>Change Name</button>
+            </center>
+    )
+}
